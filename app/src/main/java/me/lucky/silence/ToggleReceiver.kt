6049d730e191cf27fd0215cd8f9fb7851b63a454
lucky <>
2021-06-14 16:01:48 +0300

    refactor

--- app/src/main/java/me/lucky/silence/ToggleReceiver.kt
+++ app/src/main/java/me/lucky/silence/ToggleReceiver.kt
@@ -5,7 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class ToggleBroadcastReceiver : BroadcastReceiver() {
+class ToggleReceiver : BroadcastReceiver() {
     companion object {
         private const val TOGGLE_ON = "me.lucky.silence.action.TOGGLE_ON"
         private const val TOGGLE_OFF = "me.lucky.silence.action.TOGGLE_OFF"
