bca8a0b06b45eb1c5ae756e1eeab3d517c2bc740
lucky <>
2021-12-30 04:04:02 +0300

    add cleanup service

---
+++ app/src/main/java/me/lucky/silence/CleanupJobService.kt
@@ -0,0 +1,15 @@
+package me.lucky.silence
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class CleanupJobService : JobService() {
+    override fun onStartJob(params: JobParameters?): Boolean {
+        AppDatabase.getInstance(this).smsFilterDao().deleteInactive()
+        return false
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        return true
+    }
+}
--- app/src/main/java/me/lucky/silence/SmsReceiver.kt
+++ app/src/main/java/me/lucky/silence/SmsReceiver.kt
@@ -1,16 +1,24 @@
 package me.lucky.silence
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.database.sqlite.SQLiteConstraintException
 import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.telephony.TelephonyManager
+import java.util.concurrent.TimeUnit
 
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 
 class SmsReceiver : BroadcastReceiver() {
+    companion object {
+        private const val JOB_ID = 1
+    }
+
     private val phoneNumberUtil by lazy { PhoneNumberUtil.getInstance() }
 
     override fun onReceive(context: Context, intent: Intent) {
@@ -48,6 +56,16 @@ class SmsReceiver : BroadcastReceiver() {
             }
             hasNumber = true
         }
-        if (hasNumber) {}
+        if (hasNumber) {
+            val jobScheduler = context
+                .getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(
+                JobInfo.Builder(JOB_ID, ComponentName(context, CleanupJobService::class.java))
+                    .setMinimumLatency(TimeUnit
+                        .SECONDS
+                        .toMillis(SmsFilterDao.INACTIVE_DURATION.toLong() + 1))
+                    .build()
+            )
+        }
     }
 }
