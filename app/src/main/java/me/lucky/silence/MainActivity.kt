c7ac55bb7677f80270e3d3664eda7988608e5a95
Joel Beckmeyer <joel@beckmeyer.us>
2024-02-29 16:17:55 -0500

    display title of current location in app

--- app/src/main/java/me/lucky/silence/MainActivity.kt
+++ app/src/main/java/me/lucky/silence/MainActivity.kt
@@ -52,16 +52,32 @@ open class MainActivity : AppCompatActivity() {
         supportFragmentManager
             .beginTransaction()
             .replace(binding.fragment.id, f)
+            .runOnCommit { setAppbarTitle(f.javaClass) }
             .commit()
 
     private fun getFragment(id: Int) = when (id) {
-        R.id.nav_main -> MainFragment()
         R.id.nav_contacted -> ContactedFragment()
         R.id.nav_groups -> GroupsFragment()
         R.id.nav_repeated -> RepeatedFragment()
         R.id.nav_messages -> MessagesFragment()
         R.id.nav_sim -> SimFragment()
         R.id.nav_extra -> ExtraFragment()
+        R.id.top_settings -> SettingsFragment()
         else -> MainFragment()
+    }
+
+    private fun setAppbarTitle(id: Class<Fragment>) {
+        binding.appBar.title = getString(
+            when (id) {
+                ContactedFragment::class.java -> R.string.contacted
+                GroupsFragment::class.java -> R.string.groups
+                RepeatedFragment::class.java -> R.string.repeated
+                MessagesFragment::class.java -> R.string.messages
+                SimFragment::class.java -> R.string.sim
+                ExtraFragment::class.java -> R.string.extra
+                SettingsFragment::class.java -> R.string.settings
+                else -> R.string.main
+            }
+        )
     }
 }
